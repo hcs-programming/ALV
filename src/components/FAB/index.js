7b874ff26541911d50e3409236def39a30df66eb
KGrimm28 <VaporGrimm28@icloud.com>
2018-02-27 09:36:40 -0500

    LeBron

---
+++ src/components/FAB/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import logo from "../../assets/logo.svg"
+import "./styles.css"
+
+function FAB() {
+  return (
+    <div className="fab--btn">
+      <img className="fab--image" src="aeroplane2.png" />
+    </div>
+  )
+}
+
+export default FAB
