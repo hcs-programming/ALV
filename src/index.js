27f87d1eb569a871d9c8822240b4455aa0772506
Scott Brady <scott@prolio.com>
2018-03-12 21:50:59 -0400

    Making it look like codecademy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
-import App from "./components/App"
+
+import ChooseClassScreen from "./screens/ChooseClassScreen"
+import MainScreen from "./screens/MainScreen"
+
+class App extends React.Component {
+  state = {
+    chosenClass: null
+  }
+
+  render() {
+    const { chosenClass } = this.state
+
+    if (chosenClass === null) {
+      return (
+        <ChooseClassScreen
+          chooseClass={chosenClass => this.setState({ chosenClass })}
+        />
+      )
+    } else {
+      return <MainScreen />
+    }
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"))
